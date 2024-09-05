cabfb24c23678777f4279f59551746809c2bd097
Mynor <kapatano3005@gmail.com>
2024-09-04 22:17:17 -0400

    chore: Add framer-motion library and update project dependencies

--- MiniProjects/src/components/Sidebar.jsx
+++ MiniProjects/src/components/Sidebar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
+import { fadeAnimation, slideAnimation } from "../config/motion";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div>
+      <AnimatePresence>
+        <motion.button {...slideAnimation("down")}>Sidebar</motion.button>
+      </AnimatePresence>
+    </div>
   )
 }
 
