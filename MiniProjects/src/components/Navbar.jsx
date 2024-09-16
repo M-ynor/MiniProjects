b2742da5de81a15d318f831205a650f44af1913f
Mynor <kapatano3005@gmail.com>
2024-09-16 18:30:25 -0400

    Refactor Navbar component, update menu toggle behavior, and improve responsive design

--- MiniProjects/src/components/Navbar.jsx
+++ MiniProjects/src/components/Navbar.jsx
@@ -1,43 +1,47 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { fadeAnimation, slideAnimation } from "../config/motion";
 import { Icon } from '@iconify/react';
-import { Sidebar } from '../components/index';
+import Sidebar from './Sidebar';
+import { slideAnimation } from '../config/motion';
 
 const menuClosed = "line-md:menu-to-close-transition";
 const menuOpen = "line-md:close-to-menu-transition";
 
 function Navbar() {
-    const [menu, menuStatus] = useState(false);
+    const [menu, setMenu] = useState(false);
 
     const toggleMenu = () => {
-        menuStatus(!menu);
+        setMenu(!menu);
     };
 
     return (
         <div className=''>
+            <motion.nav {...slideAnimation("down")} className={`w-full h-16 sm:h-24 flex items-center justify-between bg-black mt-3`}>
+                <motion.div className='flex items-center h-fit'>
+                    <p className='text-4xl text-Green font-semibold h-fit pl-5 sm:text-[4.5vh] sm:pl-10 '>MiniProjects</p>
+                </motion.div>
+                <motion.button
+                    className={`flex mr-8 w-fit h-fit justify-center items-center text-Green`}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.8 }}
+                    onClick={toggleMenu}
+                >
+                    <Icon key={menu ? 'closed' : 'open'} icon={menu ? menuClosed : menuOpen} width="40" height="35" />
+                </motion.button>
+            </motion.nav>
             <AnimatePresence>
-                <motion.nav {...slideAnimation("down")} className={`w-full h-16 flex items-center justify-between bg-black mt-3`}>
-                    <motion.div className='flex items-center h-fit'>
-                        <p className='text-4xl text-Green font-semibold h-fit pl-4'>MiniProjects</p>
-                    </motion.div>
-                    <motion.button
-                        className={`flex mr-8 w-fit h-fit  justify-center items-center text-Green`}
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 0.8 }}
-                        onClick={toggleMenu}
+                {menu && (
+                    <motion.div
+                        key="menu"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0, transition: { duration: 0.5 } }}
+                        className='fixed inset-0 z-50 flex w-fit'
                     >
-                        <Icon key={menu ? 'closed' : 'open'} icon={menu ? menuClosed : menuOpen} width="40" height="35" />
-                    </motion.button>
-                </motion.nav>
-                    {menu ? (
-                        <motion.div {...slideAnimation("left")}
-                            className='fixed inset-0 z-50 flex w-fit'
-                        >
-                            <div className='absolute bg-black'></div>
-                            <Sidebar />
-                        </motion.div>
-                    ) : null}
+                        <div className='absolute bg-black'></div>
+                        <Sidebar />
+                    </motion.div>
+                )}
             </AnimatePresence>
         </div>
     );
