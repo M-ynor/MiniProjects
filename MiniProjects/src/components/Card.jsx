6d14748c51b16628e030d6a169a9582b903ea8cf
Mynor <kapatano3005@gmail.com>
2024-12-02 17:33:13 -0400

    Refactor Card component to remove unused animations and correct typo in fallback text

--- MiniProjects/src/components/Card.jsx
+++ MiniProjects/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { SunnyContext } from '../config/SunnyContext.jsx';
 import { useContext } from 'react';
-import { fadeAnimationAfter, slideAnimation, transition } from '../config/motion.js';
+import { slideAnimation } from '../config/motion.js';
 
 const moonToSunny = "line-md:sunny-outline-to-moon-loop-transition";
 const sunnyToMoon = "line-md:moon-to-sunny-outline-loop-transition";
@@ -20,7 +20,7 @@ function Card({ image, title, description, GoToprojectUrl }) {
         alt={`${image? {image} : 'image not avaiable'}`} 
         className={`text-center object-cover w-full h-[60%] ${isSunny? 'text-white' : 'text-black'} transition-colors duration-300`}
         />
-        <h1 className={`w-[90%] text-start font-bold 2xl:text-lg ${isSunny ? 'text-white' : 'text-black'}`}>{title? title : 'title not avaiable'}</h1>
+        <h1 className={`w-[90%] text-start font-bold 2xl:text-lg ${isSunny ? 'text-white' : 'text-black'}`}>{title? title : 'title not available'}</h1>
         <p className='w-[90%] text-start 2xl:text-lg text-gray-400'>{description? description : 'Description not avaiable'}</p>
         <button onClick={GoToprojectUrl} className='hover:bg-[#1ED760] m-1 ml-1 text-center rounded-lg w-[90%] 2xl:h-10 2xl:text-lg font-semibold bg-Green'>Launch Project {`>`} </button>
       </motion.div>
